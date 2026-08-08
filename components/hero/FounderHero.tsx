d7d395aad48a098ecc17115b4c169ea8d1365cf7
Joseph Jilovec <123526806+josephjilovec@users.noreply.github.com>
2026-08-08 07:56:03 -0700

    Use ten-project portfolio in founder hero

--- components/hero/FounderHero.tsx
+++ components/hero/FounderHero.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { ventureCategories, ventures } from "@/lib/ventures";
+import { ventureCategories, ventures } from "@/lib/portfolio";
 
 export function FounderHero() {
   return (
