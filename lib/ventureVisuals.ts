8d3666ff229a5df5fba2fad620641bb8f3e32a1a
Joseph Jilovec <123526806+josephjilovec@users.noreply.github.com>
2026-08-09 05:34:39 -0700

    Centralize venture portfolio artwork

---
+++ lib/ventureVisuals.ts
@@ -0,0 +1,24 @@
+export const ventureSignalArt: Record<string, string> = {
+  "my-healthy-aura": "/constellation-art/aura-map.svg",
+  "swift-deal-solutions": "/constellation-art/swift-map.svg",
+  "dj-hotwax": "/constellation-art/hotwax-map.svg",
+  "vanta-helix": "/constellation-art/vanta-map.svg",
+  "cannacore-seeds": "/constellation-art/cannacore-map.svg",
+  "snarklogic": "/constellation-art/snark-map.svg",
+  "alder-and-meridian": "/constellation-art/alder-map.svg",
+  "united-american-future": "/constellation-art/uaf-map.svg",
+  "world-forward-foundation": "/constellation-art/wff-map.svg",
+  "givewise-insights": "/constellation-art/givewise-map.svg",
+  "advibe-central": "/constellation-art/advibe-map.svg",
+  "kinetic-botanicals": "/constellation-art/kinetic-botanicals-map.svg",
+};
+
+export const venturePreviewArt: Record<string, string> = {
+  "my-healthy-aura": "/constellation-preview/aura-site-preview.svg",
+  "swift-deal-solutions": "/constellation-preview/swift-site-preview.svg",
+  "dj-hotwax": "/constellation-preview/hotwax-site-preview.svg",
+  "vanta-helix": "/constellation-preview/vanta-site-preview.svg",
+  "cannacore-seeds": "/constellation-preview/cannacore-site-preview.svg",
+  "snarklogic": "/constellation-preview/snark-site-preview.svg",
+  "alder-and-meridian": "/constellation-preview/alder-site-preview.svg",
+};
