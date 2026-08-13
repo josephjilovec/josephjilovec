d2eabe6751f395235860136279cceb09479e6a17
Joseph Jilovec <123526806+josephjilovec@users.noreply.github.com>
2026-08-13 14:42:39 -0700

    Build founder blueprint layout

--- components/home/BlueprintSection.tsx
+++ components/home/BlueprintSection.tsx
@@ -1,1 +1,3 @@
-export function BlueprintSection(){return <section id="founder">Founder blueprint</section>}
+import Image from "next/image";
+import { FounderCopy } from "./FounderCopy";
+export function BlueprintSection(){return <section id="founder" className="section home-anchor"><div className="founder-intro-layout"><div className="founder-intro-portrait"><Image src="/images/joseph-founder.png" alt="Joseph Jilovec" fill sizes="(max-width:900px) 100vw,42vw"/><div className="founder-portrait-caption"><span>Phoenix, Arizona</span><span>Joseph Jilovec</span></div></div><div className="founder-intro-copy"><FounderCopy/></div></div></section>}
